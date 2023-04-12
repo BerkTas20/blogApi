88d3bd593534c5fca9446225f81786f5d978869c
Berk <70212751+BerkTas20@users.noreply.github.com>
2023-04-12 22:19:43 +0300

    fixed

--- src/main/java/com/berktas/blogApi/model/entity/Post.java
+++ src/main/java/com/berktas/blogApi/model/entity/Post.java
@@ -11,25 +11,26 @@ import java.util.List;
 @Setter
 @Entity
 public class Post extends AbstractTimestampEntity {
+
     private String title;
     private String description;
     private Integer views;
+
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "comment_id", referencedColumnName = "id")
     List<Comment> comments;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category;
 
     @ManyToMany
-    @JoinTable(
-            name = "post_tags",
-            joinColumns = @JoinColumn(name = "post_id"),
-            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    @JoinTable(name = "post_tags", joinColumns = @JoinColumn(name = "post_id"), inverseJoinColumns = @JoinColumn(name = "tag_id"))
     private List<Tag> tags;
     @ManyToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     private User user;
+
     @OneToOne //burayı tekrar kontrol et
     private Like like;
 }
