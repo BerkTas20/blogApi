b16d114fee58825a38394e3f2fee7f69987f48cd
Berk <70212751+BerkTas20@users.noreply.github.com>
2023-05-17 17:22:20 +0300

    added validation

--- src/main/java/com/berktas/blogApi/controller/requests/SaveAndUpdateUserRequest.java
+++ src/main/java/com/berktas/blogApi/controller/requests/SaveAndUpdateUserRequest.java
@@ -3,6 +3,10 @@ package com.berktas.blogApi.controller.requests;
 import com.berktas.blogApi.enums.Role;
 import lombok.*;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @Builder
 @Setter
@@ -10,10 +14,18 @@ import lombok.*;
 @NoArgsConstructor
 public class SaveAndUpdateUserRequest {
 
+        @NotBlank(message = "First name cannot be blank")
         private String firstName;
+
+        @NotBlank(message = "Last name cannot be blank")
         private String lastName;
         private String username;
+
+        @Pattern(regexp = "\\d{10}", message = "Phone must be a 10-digit number")
         private String phone;
+
+        @NotBlank(message = "Email cannot be blank")
+        @Email(message = "Invalid email format")
         private String email;
         private String password;
         private Role role;
--- src/main/java/com/berktas/blogApi/controller/requests/SavePostRequest.java
+++ src/main/java/com/berktas/blogApi/controller/requests/SavePostRequest.java
@@ -6,11 +6,19 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class SavePostRequest {
+
+    @NotBlank
     private String description;
+
+    @NotBlank
+    @Size(max = 100, message = "Title cannot exceed 100 characters")
     private String title;
 }
